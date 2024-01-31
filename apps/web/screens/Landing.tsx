5fd1625b3196d75447716c99dd7e59c565ec9856
sumanbiswas7 <textsumanb@gmail.com>
2024-01-31 20:45:39 +0530

    update: make tracks appear side by side responsive

--- apps/web/screens/Landing.tsx
+++ apps/web/screens/Landing.tsx
@@ -1,35 +1,42 @@
-
 import { Track } from "@repo/store";
 import { getFunction } from "@repo/common";
 import { TrackCard } from "@repo/ui/components";
 import Link from "next/link";
 import { AppbarClient } from "../components/AppbarClient";
+import classes from "./Landing.module.css";
 
 async function getTracks() {
-    const getTrancksFn = getFunction("getTracks")
-    try {
-        const tracks: any = await getTrancksFn();
-        console.log(tracks);
-        return tracks.data.tracks || [];
-    } catch(e) {
-        return [];
-    }
+  const getTrancksFn = getFunction("getTracks");
+  try {
+    const tracks: any = await getTrancksFn();
+    console.log(tracks);
+    return tracks.data.tracks || [];
+  } catch (e) {
+    return [];
+  }
 }
 
 export async function Landing() {
-    const tracks = await getTracks()
+  const tracks = await getTracks();
 
-    return <div>
-        <AppbarClient />
-        <div className="flex justify-center pt-4">
-            <div className="text-white text-4xl p-2 max-w-screen-md">
-                Learning Paths
-            </div>
+  return (
+    <div>
+      <AppbarClient />
+      <div className="flex justify-center pt-4">
+        <div className="text-white text-4xl p-2 max-w-screen-md">
+          Learning Paths
         </div>
-            {tracks.map((t: Track) => <div style={{padding: 20}} className="flex justify-center pt-1">
-                <Link className="max-w-screen-md w-full" href={`/tracks/${t.id}`}>
-                    <TrackCard track={t} />
-                </Link>
-            </div>)}
+      </div>
+
+      <div className={classes.grid_container}>
+        {tracks.map((t: Track) => (
+          <div className="flex justify-center pt-1">
+            <Link className="max-w-screen-md w-full" href={`/tracks/${t.id}`}>
+              <TrackCard track={t} />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
+  );
 }
