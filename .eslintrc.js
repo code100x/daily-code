d58e52ffed7effb3effda7e1cb261a2e69e80a07
Kirat <harkirat96@gmail.com>
2024-02-01 14:19:59 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
+
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
   extends: ["@repo/eslint-config/library.js"],
