e4a28eca4dfbb0edc6fcb6481b4cd0c5a98658d4
Aryaman Gupta <144788392+Aryam2121@users.noreply.github.com>
2024-03-03 01:06:56 +0530

    Update page.tsx

--- apps/web/app/admin/page.tsx
+++ apps/web/app/admin/page.tsx
@@ -2,10 +2,12 @@
 
 import { Admin } from "@repo/ui/pages";
 import { AppbarClient } from "../../components/AppbarClient";
+import { ThemeProvider } from "../../components/ThemeContext";
 
 export default function () {
   return (
     <>
+      <ThemeProvider/>
       <AppbarClient />
       <Admin />
     </>
