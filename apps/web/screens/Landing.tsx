51b1ee6710914f5d882644a5addbc823e6afcdbe
root <root@MSI>
2024-03-23 00:39:15 +0530

    Added Category

--- apps/web/screens/Landing.tsx
+++ apps/web/screens/Landing.tsx
@@ -6,6 +6,7 @@ async function getTracks() {
   const getTracksFn = getFunction("getTracks");
   try {
     const tracks: any = await getTracksFn();
+    console.log(tracks);
     return tracks.data.tracks || [];
   } catch (e) {
     return [];
@@ -14,7 +15,6 @@ async function getTracks() {
 
 export async function Landing() {
   const tracks = await getTracks();
-  console.log(tracks);
   return (
     <div>
       <AppbarClient />
