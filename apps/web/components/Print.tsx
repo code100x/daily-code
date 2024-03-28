df4bb9aabab7fb9a61646c4cf053e222929abb40
Dev Manwani <36275192+Devmanwani@users.noreply.github.com>
2024-03-28 07:08:05 +0530

    Update Print.tsx

--- apps/web/components/Print.tsx
+++ apps/web/components/Print.tsx
@@ -1,16 +1,30 @@
 "use client";
+
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export function Print() {
-  // Some hack for strict mode
-  let opened = false;
   useEffect(() => {
+    let opened = false;
     if (opened) return;
+
+    
     document.querySelectorAll("details").forEach((e) => (e.open = true));
+
+    
     print();
+
+    
     opened = true;
-    redirect("/");
+  }, []);
+
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      redirect("/");
+    }, 1000); 
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return null;
