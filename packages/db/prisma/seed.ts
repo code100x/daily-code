dde21e6c2a6f0a4593c8c00f92a7f9171d35344f
harkirat <harkirat96@gmail.com>
2024-04-02 21:37:25 +0530

    Added prisma schema

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,33 @@
+import db from "../src";
+
+async function main() {
+  const track = await db.track.create({
+    data: {
+      title: "Track 1",
+      description: "Track 1 description",
+      image: "https://ideogram.ai/api/images/direct/8WZ67gBuQ8upuCvVHTWU4g.jpg",
+      problems: {
+        create: {
+          problem: {
+            create: {
+              title: "Problem 1",
+              description: "Problem 1 description",
+              notionDocId: "Step-11-Initialize-balances-on-signup-b2a919bd555a47579d848144e8190553",
+              type: "Blog",
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
