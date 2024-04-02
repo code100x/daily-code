3715f966f44c59245f94665a304bde1a77e4d842
Nimit Haria <nimitharia007@gmail.com>
2024-04-03 03:16:19 +0530

    modified seed.ts file to have 2 tracks and some problems

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,24 +1,93 @@
 import db from "../src";
 
-async function main() {
-  const track = await db.track.create({
-    data: {
-      title: "Track 1",
-      description: "Track 1 description",
-      image: "https://ideogram.ai/api/images/direct/8WZ67gBuQ8upuCvVHTWU4g.jpg",
-      problems: {
-        create: {
+const seedData = [
+  {
+    title: "Typescript",
+    description:
+      "Learn about strongly typed languages, Typescript and how you can integrate it into your existing javascript codebase.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png",
+    problems: {
+      create: [
+        {
           problem: {
             create: {
-              title: "Problem 1",
-              description: "Problem 1 description",
-              notionDocId: "Step-11-Initialize-balances-on-signup-b2a919bd555a47579d848144e8190553",
+              description: "The tsc compiler",
+              title: "The tsc compiler",
               type: "Blog",
+              notionDocId: "c537d8ae0dc9424fba168f04d024b797",
             },
           },
         },
-      },
+        {
+          problem: {
+            create: {
+              description: "What is Typescript",
+              title: "What is Typescript",
+              type: "Blog",
+              notionDocId: "3843927cfd3c44a6b0e6107a66bd10a3",
+            },
+          },
+        },
+        {
+          problem: {
+            create: {
+              description: "Types of Languages",
+              title: "Types of Languages",
+              type: "Blog",
+              notionDocId: "Step-1-Types-of-languages-24f2476c2b9c4c97a51bfef919dd39e6",
+            },
+          },
+        },
+      ],
     },
+  },
+  {
+    title: "Authentication",
+    description: "How to implement authentication in your website.",
+    image: "https://ideogram.ai/api/images/direct/C_U7Xo9YQiib6JjbreU2VQ.png",
+    problems: {
+      create: [
+        {
+          problem: {
+            create: {
+              description: "Authentication using cookies (Part 1)",
+              title: "Authentication using cookies (Part 1)",
+              type: "Blog",
+              notionDocId: "Authentication-using-cookies-Part-1-5b2bc62899f34720ad888df4873c966e",
+            },
+          },
+        },
+        {
+          problem: {
+            create: {
+              description: "Authentication using jwt + localstorage",
+              title: "Authentication using jwt + localstorage",
+              type: "Blog",
+              notionDocId: "Authentication-using-jwt-localstorage-8a6be0a741074bf2b79cc610126a1a96",
+            },
+          },
+        },
+        {
+          problem: {
+            create: {
+              description: "What is authentication?",
+              title: "What is authentication?",
+              type: "Blog",
+              notionDocId: "What-is-authentication-e8fb0241878349d2bd0a6115d8ca1071",
+            },
+          },
+        },
+      ],
+    },
+  },
+];
+
+async function main() {
+  seedData.forEach(async (track: any) => {
+    await db.track.create({
+      data: track,
+    });
   });
 }
 
