aac7fc6821cb07918e2bb21c15758db8d802818a
mvking5464 <mvking5464@gmail.com>
2024-04-06 20:48:41 +0530

    Cleaning and wraping inside try_catch

--- apps/web/pages/api/revalidate.ts
+++ apps/web/pages/api/revalidate.ts
@@ -1,20 +1,17 @@
-// https://<your-site.com>/api/revalidate?secret=<token>
-// http://localhost/api/revalidate?secret=MySecretPassword
-// http://localhost/api/revalidate?path=/&secret=MySecretPassword //add more path for this
-
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const fromQuery = req.query.secret;
   const fromEnv = process.env.MY_SECRET_TOKEN;
   if (fromQuery != fromEnv) {
-    console.log(process.env.MY_SECRET_TOKEN);
-    console.log(req.query.secret);
     return res.status(401).json({ message: "Invalid token" });
   }
+  try {
+    const path = req.query.path as string;
 
-  const path = req.query.path as string;
-
-  await res.revalidate(path);
-  return res.json({ revalidated: true });
+    await res.revalidate(path);
+    return res.json({ revalidated: true });
+  } catch (e) {
+    return res.status(500).send("Error revalidating");
+  }
 }
