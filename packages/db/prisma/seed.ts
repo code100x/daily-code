f20a0a2b0f9fd8cfdf9a0f0f6dec9120f030b8f6
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-10 03:56:08 +0530

    test-commit

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -6,6 +6,11 @@ async function main() {
       title: "Track 1",
       description: "Track 1 description",
       image: "https://ideogram.ai/api/images/direct/8WZ67gBuQ8upuCvVHTWU4g.jpg",
+      category: {
+        create: {
+          category: "Category 1",
+        },
+      },
       problems: {
         create: {
           problem: {
@@ -14,12 +19,29 @@ async function main() {
               description: "Problem 1 description",
               notionDocId: "Step-11-Initialize-balances-on-signup-b2a919bd555a47579d848144e8190553",
               type: "Blog",
+              mcqQuestions: {
+                create: [
+                  {
+                    question: "Question 1",
+                    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+                    correctOption: "Option 2",
+                  },
+                  {
+                    question: "Question 2",
+                    options: ["Option A", "Option B", "Option C", "Option D"],
+                    correctOption: "Option C",
+                  },
+                ],
+              },
             },
           },
+          sortingOrder: 1, // Required for the TrackProblems model
         },
       },
     },
   });
+
+  console.log("Track created:", track);
 }
 
 main()
