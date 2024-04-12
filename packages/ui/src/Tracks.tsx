9386ef8a66fe001fc366e372dcd5fb60ec5e09d4
MAYANK AGRAWAL <mayk03jun@gmail.com>
2024-04-12 09:50:22 +0530

    fixes the ui

--- packages/ui/src/Tracks.tsx
+++ packages/ui/src/Tracks.tsx
@@ -28,11 +28,11 @@ export const Tracks = ({ tracks }: { tracks: Tracks[] }) => {
   }, [selectedCategory]);
   return (
     <div>
-      <ul className="p-8 md:20 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-2 w-full">
+      <ul className="p-8 md:20 grid grid-cols-1 gap-x-6 gap-y-8 place-items-center lg:grid-cols-2 w-full">
         {filteredTracks.map((t) => (
-          <li key={t.id} className="flex justify-center">
+          <li key={t.id} className="max-w-screen-md w-full">
             {t.problems.length > 0 ? (
-              <Link className="max-w-screen-md w-full block" href={`/tracks/${t.id}/${t.problems[0]?.id}`}>
+              <Link className="w-full" href={`/tracks/${t.id}/${t.problems[0]?.id}`}>
                 <TrackCard track={t} />
               </Link>
             ) : (
