e0c15f0592ecee16fcf9e1e3f1d7bbe7158ab7d0
Sujith Thriumalaisamy <sujithmasi1267@gmail.com>
2024-04-12 14:20:35 +0530

    [Fixed]: Fixed some types
    Updated some type errors

--- apps/web/app/api/createTrack/route.ts
+++ apps/web/app/api/createTrack/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { randomUUID } from "crypto";
 import { NotionAPI } from "notion-client";
 import db from "../../../../../packages/db/src";
-import { Problem } from "../../../../../packages/store/src/atoms";
+import { Problem, Track } from "../../../../../packages/store/src/atoms";
 
 export async function POST(req: NextRequest) {
   const notionLink = req.nextUrl.searchParams.get("link");
@@ -28,9 +28,9 @@ const extractInfoFromNotionLink = (link: string) => {
   var match = link.match(pattern);
 
   if (match && match[1]) {
-    var title = match[1].replace(/-/g, " ");
-    var uuid = match[2];
-    return { title: title, uuid: uuid };
+    var title: string = match[1].replace(/-/g, " ");
+    var id: string = match[2] || "";
+    return { title: title, id: id };
   } else {
     return null;
   }
@@ -38,8 +38,8 @@ const extractInfoFromNotionLink = (link: string) => {
 
 const feedNotionToDB = async (notionlink: string, description: string, image: string, type: string) => {
   const notion = new NotionAPI();
-  var { title, uuid } = extractInfoFromNotionLink(notionlink);
-  const pages = await notion.getPage(uuid);
+  var { title, id } = extractInfoFromNotionLink(notionlink);
+  const pages = await notion.getPage(id);
   const data = await notion.getBlocks(Object.keys(pages.block));
   const problems: Problem[] = [];
   var count = Object.keys(data.recordMap.block).length;
