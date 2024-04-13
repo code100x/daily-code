4fdc128ae952eb04c009c680f45326b888126fd0
Shivam <shivamlather5@gmail.com>
2024-04-13 16:21:56 +0530

    Made it only select category

--- packages/ui/src/Categories.tsx
+++ packages/ui/src/Categories.tsx
@@ -16,24 +16,23 @@ export const Categories = ({ categories }: { categories: { category: string }[] 
   const [selectedCategory, setSelectedCategory] = useRecoilState(category);
   return (
     <div className="flex justify-center items-center">
-      <div className="flex  border-2 dark:border-slate-700 text-bg-slate-700 rounded-full py-1 px-3">
+      <div className="flex  border-2 dark:border-slate-700 text-bg-slate-700 rounded-full py-2 px-3">
         <DropdownMenu>
-          <DropdownMenuTrigger>Select Category</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <div className="bg-background p-2 border-2 dark:border-slate-700 rounded-xl flex flex-col  items-center">
-              {categories.map((category) => (
-                <DropdownMenuItem
-                  onClick={() => handleFilterButton(category.category)}
-                  className={
-                    selectedCategory == category.category
-                      ? "bg-gray-100 flex justify-center dark:bg-slate-700 w-full  font-bold font-2xl"
-                      : "font-semibold  py-2 w-full flex justify-center"
-                  }
-                >
-                  {category.category}
-                </DropdownMenuItem>
-              ))}
-            </div>
+          <DropdownMenuTrigger className="focus:ring-0">{selectedCategory || "Select Category"}</DropdownMenuTrigger>
+
+          <DropdownMenuContent className="bg-background p-2 border-2 focus:ring-0 dark:border-slate-700 rounded-xl flex flex-col  items-center">
+            {categories.map((category) => (
+              <DropdownMenuItem
+                onClick={() => handleFilterButton(category.category)}
+                className={
+                  selectedCategory == category.category
+                    ? "bg-gray-100 flex justify-center focus:ring-0 dark:bg-slate-700 w-full  font-bold font-2xl"
+                    : "font-semibold  py-2 w-full flex justify-center focus:ring-0"
+                }
+              >
+                {category.category}
+              </DropdownMenuItem>
+            ))}
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
