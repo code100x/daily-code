1b2ae3320ee4170cd743328bcc834845acca9296
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2024-04-14 11:59:09 +0530

    Made Categories.tsx responsive

--- packages/ui/src/Categories.tsx
+++ packages/ui/src/Categories.tsx
@@ -1,29 +1,59 @@
 "use client";
 import { Button } from "./shad/ui/button";
 import { useRecoilState } from "recoil";
+import { useEffect, useState } from "react";
 import { category } from "@repo/store";
 
 export const Categories = ({ categories }: { categories: { category: string }[] }) => {
-  function handleFilterButton(category: string) {
-    if (category == selectedCategory) {
-      setSelectedCategory("");
-    } else {
-      setSelectedCategory(category);
-    }
-  }
+  const [isMobile, setIsMobile] = useState(false);
   const [selectedCategory, setSelectedCategory] = useRecoilState(category);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1536);
+    };
+
+    handleResize(); // Call the function once to set initial state
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove the event listener when component unmounts
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  function handleFilterButton(category: string) {
+    setSelectedCategory((prevCategory) => (prevCategory === category ? "" : category));
+  }
+
   return (
-    <div className="flex justify-evenly mx-auto border-2 rounded-full py-1 w-2/3">
-      {categories.map((category) => (
-        <Button
-          key={category.category}
-          variant="ghost"
-          onClick={() => handleFilterButton(category.category)}
-          className={selectedCategory == category.category ? "bg-gray-100 dark:bg-slate-700" : ""}
+    <div
+      className={`flex justify-center mx-auto border-2 rounded-full py-1 ${!isMobile ? "flex-row w-2/3" : "flex-col w-fit"}`}
+    >
+      {!isMobile ? (
+        categories.map((category) => (
+          <Button
+            key={category.category}
+            variant="ghost"
+            onClick={() => handleFilterButton(category.category)}
+            className={selectedCategory === category.category ? "bg-gray-100 dark:bg-slate-700" : ""}
+          >
+            {category.category}
+          </Button>
+        ))
+      ) : (
+        <select
+          className="bg-transparent justify-center p-2 mx-1"
+          name="category"
+          id="category"
+          onChange={(e) => handleFilterButton(e.target.value)}
         >
-          {category.category}
-        </Button>
-      ))}
+          {categories.map((category) => (
+            <option key={category.category} value={category.category}>
+              {category.category}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 };
