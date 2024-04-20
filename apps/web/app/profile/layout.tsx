ebf60cd7acc5a15e19de7946c59d573973b432c0
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-04-20 23:10:27 +0530

    Added the User Drop Down and also added the profile page

---
+++ apps/web/app/profile/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { redirect } from "next/navigation";
+import { getAllTracks } from "../../components/utils";
+import { AppbarClient } from "../../components/AppbarClient";
+import ProfileSidebar from "../../components/ProfileSidebar";
+import ProfileChilldren from "../../components/ProfileChildren";
+
+export const metadata: Metadata = {
+  title: "Dailycode",
+  description: "Code daily",
+};
+
+const session = await getServerSession(authOptions);
+if (!session || !session?.user) {
+  redirect("/auth");
+}
+const tracks = await getAllTracks();
+
+export default function ProfileLayout({ children }: { children: React.ReactNode }): JSX.Element {
+  return (
+    <div className="min-h-screen bg-background font-sans antialiased">
+      <AppbarClient tracks={tracks} />
+      <div className="max-w-screen-xl flex p-4 m-auto">
+        <div className="p-2 w-full grid grid-cols-8 gap-2">
+          <ProfileSidebar />
+          <ProfileChilldren>{children}</ProfileChilldren>
+        </div>
+      </div>
+    </div>
+  );
+}
