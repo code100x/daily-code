104c878d91d425c40a1c1c49526e6f900e96ea40
Harshit <harshitd891@gmail.com>
2024-04-26 06:16:04 +0530

    UI improvements for sign-in page

--- apps/web/components/Signin.tsx
+++ apps/web/components/Signin.tsx
@@ -4,6 +4,8 @@ import GithubIcon from "../app/assets/github.svg";
 import { signIn, useSession } from "next-auth/react";
 import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { ModeToggle } from "../../../packages/ui/src/ModeToggle";
 const Signin = () => {
   const session = useSession();
   const router = useRouter();
@@ -19,57 +21,75 @@ const Signin = () => {
   }, [redirected, session, router]);
 
   return (
-    <div className="flex bg-black">
-      <div className="w-full md:w-2/5 bg-black flex justify-center items-center h-screen max-sm:hidden max-md:hidden">
-        <div>
-          <h1 className="text-4xl font-bold mb-4 text-white">Daily Code</h1>
-          <div className="grid grid-cols-3 gap-4">
-            {[...Array(9)].map((_, index) => (
-              <div key={index} className="opacity-50">
-                <i className="fas fa-arrow-down fa-3x"></i>
-              </div>
-            ))}
+    <>
+      <div className="bg-zinc-50 dark:bg-zinc-950 p-3 py-3 flex justify-center border-b shadow-md sticky top-0 z-50">
+        <div className="max-w-screen-xl flex justify-between w-full">
+          <Link href={"/"}>
+            <div className="dark:text-zinc-100 text-zinc-950 text-2xl font-semibold">DailyCode</div>
+          </Link>
+          <div className="flex items-center gap-2">
+            <ModeToggle />
           </div>
         </div>
       </div>
-      <div className="w-full h-screen md:w-3/5 bg-gray-900 flex justify-center items-center">
-        <div className="w-full max-w-md">
-          <div className="p-5">
-            <h2 className="text-2xl font-semibold mb-2 text-white text-center">Log In</h2>
+      <div className="flex bg-black h-[91.5vh]">
+        <div className="w-full md:w-2/5 bg-zinc-200 dark:bg-zinc-950 flex justify-center items-center h-[91.5vh] max-sm:hidden max-md:hidden">
+          <div>
+            <h1 className="text-4xl font-bold mb-4 dark:text-zinc-100 text-zinc-950">Daily Code</h1>
+            <div className="grid grid-cols-3 gap-4">
+              {[...Array(9)].map((_, index) => (
+                <div key={index} className="opacity-50">
+                  <i className="fas fa-arrow-down fa-3x"></i>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className=" mb-4  justify-center py-1 sm:px-6 lg:px-8 ">
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-              <div className="bg-white py-12 px-4 shadow sm:rounded-lg sm:px-10">
-                <div className="flex flex-col items-center justify-center gap-4">
-                  <p className="font-normal text-2xl text-gray-900">Welcome</p>
-
-                  <p className="font-light text-sm text-gray-600">Log in to continue to DailyCode.</p>
-                  <button
-                    type="submit"
-                    className="w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 "
-                    onClick={async () => {
-                      await signIn("google");
-                    }}
-                  >
-                    <img src={GoogleIcon.src} className="w-5 h-5 mr-2" />
-                    Continue with Google
-                  </button>
-                  <button
-                    className="w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 -mt-2"
-                    onClick={async () => {
-                      await signIn("github");
-                    }}
-                  >
-                    <img src={GithubIcon.src} className="w-5 h-5 mr-2" />
-                    Continue with Github
-                  </button>
+        </div>
+        <div className="w-full h-[91.5vh] md:w-3/5 bg-gray-900 flex justify-center items-center">
+          <div className="w-full max-w-md">
+            <div className="p-5">
+              <h2 className="text-2xl font-semibold mb-2 text-white text-center">Log In</h2>
+            </div>
+            <div className=" mb-4  justify-center py-1 sm:px-6 lg:px-8 ">
+              <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                  <div
+                    className="absolute inset-0 bg-gradient-to-r bg-zinc-950 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                  </div>
+                  <div className="relative px-4 py-10 bg-background shadow-lg sm:rounded-3xl sm:p-16">
+                    <div className="w-full flex justify-center">
+                      <div className="w-full flex flex-col items-center justify-center gap-4">
+                        <p className="font-normal text-2xl text-gray-900 dark:text-slate-300">Welcome</p>
+                        <p className="font-light text-sm text-gray-600 dark:text-slate-200">Log in to continue to DailyCode.</p>
+                        <button
+                          type="submit"
+                          className="w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md text-gray-900 dark:text-slate-200 hover:bg-gray-200  dark:hover:bg-gray-900 focus:outline-none "
+                          onClick={async () => {
+                            await signIn("google");
+                          }}
+                        >
+                          <img src={GoogleIcon.src} className="w-5 h-5 mr-2" />
+                          Continue with Google
+                        </button>
+                        <button
+                          className="w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md text-gray-600 dark:text-slate-200 hover:bg-gray-200 dark:hover:bg-gray-900  focus:outline-none -mt-2"
+                          onClick={async () => {
+                            await signIn("github");
+                          }}
+                        >
+                          <img src={GithubIcon.src} className="w-5 h-5 mr-2 dark:bg-gray-400 dark:rounded-full" />
+                          Continue with Github
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export { GithubIcon };
