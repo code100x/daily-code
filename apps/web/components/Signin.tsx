d0497616ca0684defc4ea3c0dba93e0562e4eaf1
Anshul Soni <102960277+anshulsoni2010@users.noreply.github.com>
2024-05-17 18:41:04 +0530

    change in button order

--- apps/web/components/Signin.tsx
+++ apps/web/components/Signin.tsx
@@ -49,19 +49,7 @@ const Signin = () => {
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-3 h-[1px] w-full" />
 
         <div className="flex flex-col space-y-4 button">
-          <button
-            className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-14 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] hover:border border-grey-200/2 transition-border transition-delay-500 duration-300"
-            type="button"
-            onClick={async () => {
-                await signIn("github");
-              }}
-          >
-            <IconBrandGithub className="h-8 w-8 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-m">
-              Continue With GitHub
-            </span>
-            <BottomGradient />
-          </button>
+         
 
           <button
             className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-14 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] hover:border border-grey-200/2 transition-border transition-delay-500 duration-300"
@@ -77,6 +65,20 @@ const Signin = () => {
             <BottomGradient />
           </button>
 
+             <button
+            className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-14 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] hover:border border-grey-200/2 transition-border transition-delay-500 duration-300"
+            type="button"
+            onClick={async () => {
+                await signIn("github");
+              }}
+          >
+            <IconBrandGithub className="h-8 w-8 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-m">
+              Continue With GitHub
+            </span>
+            <BottomGradient />
+          </button>
+            
           <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center">
             By signing in, you agree to our{" "}
             <a href="https://app.100xdevs.com/tnc">
