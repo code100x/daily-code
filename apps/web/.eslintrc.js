b4d2bf51285053fa5660870e38798f91125595ed
iamnithishraja <153272518+iamnithishraja@users.noreply.github.com>
2024-05-22 13:41:05 +0530

    removed web storybook

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js", "plugin:storybook/recommended"],
+  extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
