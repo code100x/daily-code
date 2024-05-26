5a2745dfe7ba0a9692280d2aac63369147eeaece
ashish98joshi <ashish98joshi@gmail.com>
2024-05-26 20:58:52 +0530

    fix: trackCard preview layout issue

--- apps/web/components/TrackPreview.tsx
+++ apps/web/components/TrackPreview.tsx
@@ -11,8 +11,8 @@ type TrackPreviewProps = {
 export function TrackPreview({ showPreview, setShowPreview, track }: TrackPreviewProps) {
   return (
     <Dialog open={showPreview} onOpenChange={() => setShowPreview(false)}>
-      <DialogContent className="max-w-2xl max-h-[90vh]">
-        <div className="p-5">
+      <DialogContent className="max-w-2xl max-h-[90vh] flex flex-col">
+        <div className="p-5 overflow-hidden">
           <div className="mb-6 relative">
             <img src={track.image} className="h-20 w-full object-cover rounded-lg" />
             <div className="text-3xl backdrop-blur-md font-black absolute w-full text-center -translate-y-14 drop-shadow-[2px_2px_var(--tw-shadow-color)] dark:shadow-stone-900 shadow-stone-100">
@@ -30,24 +30,24 @@ export function TrackPreview({ showPreview, setShowPreview, track }: TrackPrevie
           <p className="mt-5 font-bold px-5 text-xl">Contents</p>
           <div className="max-h-[40vh] overflow-y-auto">
             {track.problems.map((topic: any, idx: number) => (
-                <Link href={`/tracks/${track.id}/${track.problems[idx]?.id}`}>
-              <div className="hover:cursor-pointer my-2 rounded-md dark:hover:bg-slate-700 hover:bg-slate-200 px-5 py-1 transition-all duration-450 scroll-smooth">
-                {topic.title}
-              </div>
+              <Link href={`/tracks/${track.id}/${track.problems[idx]?.id}`} key={idx}>
+                <div className="hover:cursor-pointer my-2 rounded-md dark:hover:bg-slate-700 hover:bg-slate-200 px-5 py-1.5 transition-all duration-450 scroll-smooth">
+                  {topic.title}
+                </div>
               </Link>
             ))}
           </div>
-          <div className="w-full flex justify-center">
-            <Link href={track.problems.length ? `/tracks/${track.id}/${track.problems[0]?.id}` : ""}>
-              <Button
-                size={"lg"}
-                className="flex items-center justify-center group"
-                onClick={(e) => e.stopPropagation()}
-              >
-                Start
-              </Button>
-            </Link>
-          </div>
+        </div>
+        <div className="w-full flex justify-center">
+          <Link href={track.problems.length ? `/tracks/${track.id}/${track.problems[0]?.id}` : ""}>
+            <Button
+              size={"lg"}
+              className="flex items-center justify-center group"
+              onClick={(e) => e.stopPropagation()}
+            >
+              Start
+            </Button>
+          </Link>
         </div>
       </DialogContent>
     </Dialog>
