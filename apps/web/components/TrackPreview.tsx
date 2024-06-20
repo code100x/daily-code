47b2cbf7dc2da97b897f4ec2eca64d4ba85c6535
Rudra Prasad <rudraofficial202@gmail.com>
2024-06-20 11:38:41 +0530

    added enter-icon

--- apps/web/components/TrackPreview.tsx
+++ apps/web/components/TrackPreview.tsx
@@ -1,25 +1,52 @@
+"use client";
 import Link from "next/link";
-import { Button } from "../../../packages/ui/src/shad/ui/button";
-import { Dialog, DialogClose, DialogContent } from "../../../packages/ui/src/shad/ui/dailog";
+import { Button } from "@repo/ui";
+import { Dialog, DialogClose, DialogContent } from "@repo/ui";
 import { ScrollArea } from "@repo/ui";
+import { EnterIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
 
 type TrackPreviewProps = {
   showPreview: boolean;
   setShowPreview: (val: boolean) => void;
   track: any;
 };
+
+const truncateDescription = (text: string, wordLimit: number) => {
+  const words = text.split(" ");
+  if (words.length > wordLimit) {
+    return words.slice(0, wordLimit).join(" ") + " ...";
+  }
+  return text;
+};
 export function TrackPreview({ showPreview, setShowPreview, track }: TrackPreviewProps) {
+  const [isMediumOrLarger, setIsMediumOrLarger] = useState(false);
+
+  const updateScreenSize = () => {
+    setIsMediumOrLarger(window.innerWidth >= 768);
+  };
+
+  useEffect(() => {
+    updateScreenSize(); // Set the initial state
+    window.addEventListener("resize", updateScreenSize); // Add resize listener
+
+    return () => {
+      window.removeEventListener("resize", updateScreenSize); // Cleanup listener on unmount
+    };
+  }, []);
+
+  const truncatedDescription = isMediumOrLarger ? track.description : truncateDescription(track.description, 15);
   return (
     <Dialog open={showPreview} onOpenChange={() => setShowPreview(false)}>
-      <DialogContent className="rounded-lg  ">
-        <div className="p-5">
+      <DialogContent className="rounded-lg">
+        <div className="p-2">
           <div className="mb-6 relative">
             <img src={track.image} className="h-20 w-full object-cover rounded-lg" />
             <div className="text-3xl backdrop-blur-md font-black absolute w-full text-center -translate-y-14 drop-shadow-[2px_2px_var(--tw-shadow-color)] dark:shadow-stone-900 shadow-stone-100">
               {track.title}
             </div>
           </div>
-          <div className=" p-2 m-2 bg-gray-200 overflow-hidden rounded-lg text-xl h-fit w-fit text-zinc-600">
+          <div className="p-2 m-2 bg-gray-200 overflow-hidden rounded-lg text-xl h-fit w-fit text-zinc-600">
             {track.categories.map((item: any, idx: number) => (
               <p key={item.category.id} className="inline-block text-sm font-extrabold  ">
                 {item.category.category}{" "}
@@ -27,15 +54,18 @@ export function TrackPreview({ showPreview, setShowPreview, track }: TrackPrevie
               </p>
             ))}
           </div>
-          <p className="pb-5 px-5">{track.description}</p>
+          <p className="pb-5 px-3">{truncatedDescription}</p>
           <hr />
-          <p className="mt-5 font-bold px-5 text-xl">Contents</p>
+          <p className="mt-5 font-bold px-3 text-xl">Contents</p>
           <ScrollArea className="h-72 rounded-md border m-2">
             <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
               {track.problems.map((topic: any, idx: number) => (
                 <Link href={`/tracks/${track.id}/${track.problems[idx]?.id}`}>
-                  <div className="hover:cursor-pointer my-2 rounded-md dark:hover:bg-slate-700 hover:bg-slate-200 px-5 py-1 transition-all duration-450 scroll-smooth">
+                  <div className="hover:cursor-pointer flex items-center justify-between my-2 rounded-md dark:hover:bg-slate-700 hover:bg-slate-200 md:px-5 py-1 transition-all duration-450 scroll-smooth">
                     <li>{topic.title}</li>
+                    <div className="pr-2">
+                      <EnterIcon />
+                    </div>
                   </div>
                 </Link>
               ))}
