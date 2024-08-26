1b5d1f53ecc70ce345a509dce4bc89ca3b9a1860
ItsFlash10 <satyamsworld@gmail.com>
2024-08-27 00:37:10 +0530

    feature: #573: Added track in the side bar and improved theme for NotionRender

--- apps/web/components/BlogAppbar.tsx
+++ apps/web/components/BlogAppbar.tsx
@@ -3,14 +3,14 @@
 import { Problem, Track } from "@prisma/client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Button, Separator } from "@repo/ui";
+import { useParams, useRouter } from "next/navigation";
+import { Button, Card, CardContent, Separator } from "@repo/ui";
 import { ModeToggle } from "./ModeToggle";
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import UserAccountDropDown from "./UserAccountDropDown";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Download, Menu, User, X } from "lucide-react";
 
 export const BlogAppbar = ({
@@ -24,6 +24,10 @@ export const BlogAppbar = ({
   const router = useRouter();
   const session = useSession();
   const user = session.data?.user;
+
+  const { trackIds }: { trackIds?: string[] } = useParams();
+  const currentTrack = trackIds ? trackIds.join("/") : "";
+
   const [isOpen, setIsOpen] = useState<boolean>(() => {
     const storedPreference = localStorage.getItem("modeToggleIsOpen");
     return storedPreference === "true" || false;
@@ -44,6 +48,39 @@ export const BlogAppbar = ({
       }
       setLastScrollY(currentScrollY);
     }
+  };
+
+  const renderTopics = () => {
+    return (
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ x: -20 }}
+          transition={{ duration: 0.3 }}
+          className={`${visible ? "translate-y-0" : "-translate-y-full"} px-6`}
+        >
+          <Card className="w-fit bg-black/10 backdrop-blur-lg border-primary/10 pt-4 max-h-[50vh] overflow-auto no-scrollbar">
+            {track?.problems?.map((problem: { id: string; title: string }, index: number) => {
+              const isDisabled = currentTrack === `${track.id}/${problem.id}`;
+              return (
+                <CardContent key={index} className={isDisabled ? "opacity-50" : ""}>
+                  <Link
+                    key={problem.id}
+                    prefetch
+                    className="max-w-screen-md w-full"
+                    href={`/tracks/${track.id}/${problem.id}`}
+                    onClick={(e) => isDisabled && e.preventDefault()}
+                  >
+                    {index + 1} - {problem.title}
+                  </Link>
+                </CardContent>
+              );
+            })}
+          </Card>
+        </motion.div>
+      </AnimatePresence>
+    );
   };
 
   useEffect(() => {
@@ -73,83 +110,88 @@ export const BlogAppbar = ({
   }, [problemIndex, router, track]);
 
   return (
-    <motion.div
-      className={`w-full justify-between z-[50] flex flex-col md:flex-row gap-2 p-6 ${visible ? "translate-y-0" : "-translate-y-full"}`}
-      initial={{ y: 0 }}
-      animate={{ y: visible ? 0 : -100 }}
-      transition={{ duration: 0.3 }}
-    >
-      <div className="flex gap-2 items-center">
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className="cursor-pointer flex gap-4 p-3 text-primary bg-black/10 backdrop-blur-lg rounded-lg border border-primary/10 items-center"
-        >
-          {isOpen ? <X className="size-6" /> : <Menu className="size-6" />}
+    <>
+      <motion.div
+        className={`w-full justify-between z-[50] flex flex-col md:flex-row gap-2 p-6 ${visible ? "translate-y-0" : "-translate-y-full"}`}
+        initial={{ y: 0 }}
+        animate={{ y: visible ? 0 : -100 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="flex gap-2 items-center">
+          {/* menu */}
+          <div
+            onClick={() => setIsOpen(!isOpen)}
+            className="cursor-pointer flex gap-4 p-3 text-primary bg-black/10 backdrop-blur-lg rounded-lg border border-primary/10 items-center"
+          >
+            {isOpen ? <X className="size-6" /> : <Menu className="size-6" />}
+          </div>
+          {/* track title */}
+          <motion.div
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", type: "spring", damping: 10 }}
+            className="flex gap-2 justify-between"
+          >
+            <div
+              className={`flex gap-4 p-2 bg-black/10 backdrop-blur-lg rounded-lg border border-primary/10 items-center transition-all duration-500 ease-in-out `}
+            >
+              <Link href={"/"} className="hidden md:flex items-center gap-4 cursor-pointer">
+                <Image
+                  src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+                  alt="Logo"
+                  width={200}
+                  height={200}
+                  className="rounded-full size-8"
+                />
+              </Link>
+              <Separator className="hidden md:flex w-0.5 h-6 bg-primary/25" />
+
+              <h4 className="tracking-tighter flex items-center md:text-lg font-medium gap-2 md:max-w-[50vw]">
+                {track.title}
+                <span className="text-primary/80 text-sm">
+                  {problemIndex + 1} of {track.problems.length}
+                </span>
+              </h4>
+            </div>
+          </motion.div>
         </div>
+
         <motion.div
           initial={{ y: -20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5, ease: "easeInOut", type: "spring", damping: 10 }}
-          className="flex gap-2 justify-between"
+          className="flex gap-2"
         >
           <div
-            className={`flex gap-4 p-2 bg-black/10 backdrop-blur-lg rounded-lg border border-primary/10 items-center transition-all duration-500 ease-in-out `}
+            className={`flex gap-2 p-2 bg-black/10 backdrop-blur-lg rounded-lg border border-primary/10 items-center transition-all duration-500 ease-in-out ${
+              isOpen ? `translate-y-0 opacity-100` : `-translate-y-32 opacity-0`
+            }`}
           >
-            <Link href={"/"} className="hidden md:flex items-center gap-4 cursor-pointer">
-              <Image
-                src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
-                alt="Logo"
-                width={200}
-                height={200}
-                className="rounded-full size-8"
-              />
+            {!user ? (
+              <Button
+                size={"default"}
+                onClick={async () => {
+                  await signIn();
+                }}
+                className="flex gap-2"
+              >
+                <User className="size-4" />
+                Login
+              </Button>
+            ) : (
+              <UserAccountDropDown />
+            )}
+            <Link href={`/pdf/${track.id}/${track.problems[problemIndex]!.id}`} target="_blank">
+              <Button className="flex gap-2" size={"default"}>
+                Download
+                <Download className="size-4" />
+              </Button>
             </Link>
-            <Separator className="hidden md:flex w-0.5 h-6 bg-primary/25" />
-
-            <h4 className="tracking-tighter flex items-center md:text-lg font-medium gap-2 md:max-w-[50vw]">
-              {track.title}{" "}
-              <span className="text-primary/80 text-sm">
-                {problemIndex + 1} of {track.problems.length}
-              </span>
-            </h4>
+            <ModeToggle />
           </div>
         </motion.div>
-      </div>
-
-      <motion.div
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, ease: "easeInOut", type: "spring", damping: 10 }}
-        className="flex gap-2"
-      >
-        <div
-          className={`flex gap-2 p-2 bg-black/10 backdrop-blur-lg rounded-lg border border-primary/10 items-center transition-all duration-500 ease-in-out ${
-            isOpen ? `translate-y-0 opacity-100` : `-translate-y-32 opacity-0`
-          }`}
-        >
-          {!user ? (
-            <Button
-              size={"default"}
-              onClick={async () => {
-                await signIn();
-              }}
-              className="flex gap-2"
-            >
-              <User className="size-4" />
-              Login
-            </Button>
-          ) : (
-            <UserAccountDropDown />
-          )}
-          <Link href={`/pdf/${track.id}/${track.problems[problemIndex]!.id}`} target="_blank">
-            <Button className="flex gap-2" size={"default"}>
-              Download
-              <Download className="size-4" />
-            </Button>
-          </Link>
-          <ModeToggle />
-        </div>
       </motion.div>
-    </motion.div>
+      {isOpen && renderTopics()}
+    </>
   );
 };
--- apps/web/components/CodeBlock.tsx
+++ apps/web/components/CodeBlock.tsx
@@ -1,32 +1,39 @@
-import React from "react";
-import "highlight.js/styles/github-dark-dimmed.css";
-import "highlight.js/styles/github.css";
+import React, { useEffect } from "react";
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+
 import { useToast } from "@repo/ui";
+
+import "highlight.js/styles/github-dark.css";
 import { Copy } from "lucide-react";
 
 export default function CodeBlock({ block }: { block: any }) {
-  const code: string = block.properties.title[0].toString();
-
   const { toast } = useToast();
 
+  const code: string = block.properties.title[0].toString();
+  hljs.registerLanguage("javascript", javascript);
+
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(code).then(() => {
+      toast({
+        description: "Copied to clipboard",
+      });
+    });
+  };
+
   return (
-    <div className="w-full overflow-auto py-2">
-      <pre
-        onClick={() => {
-          navigator.clipboard.writeText(code).then(() => {});
-          toast({
-            description: "Copied to clipboard",
-          });
-        }}
-        className={`rounded-lg bg-secondary/50 justify-between flex lg:w-[35vw] p-6 relative group border border-primary/5 cursor-pointer `}
-      >
-        <code className="text-primary font-mono overflow-auto">{code}</code>
-        <div
-          className={`flex flex-col absolute top-4 right-4 text-primary/50 lg:opacity-25 group-hover:opacity-100 transition-all duration-300`}
-        >
-          <Copy className="size-4 text-primary/50" />
-        </div>
+    <div className="w-full relative">
+      <div className="absolute rounded-s-md w-1 inset-y-0 bg-gradient-to-b from-blue-400 to-blue-700" />
+      <pre className="rounded-md text-sm sm:text-base !bg-[#151515] px-4 sm:px-6 md:px-8 whitespace-pre-wrap break-word flex justify-between">
+        <code className="language-javascript">{code}</code>
       </pre>
+      <button className="text-gray-500 p-2 absolute top-2 right-2 hover:opacity-80" onClick={handleCopyClick}>
+        <Copy className="size-5 text-primary/50" />
+      </button>
     </div>
   );
 }
