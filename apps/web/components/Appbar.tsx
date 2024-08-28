df993e309760de3d6466b9b135e5841c804362e6
S. Adithiya <93365795+Adithiya5Hub@users.noreply.github.com>
2024-08-28 13:33:35 +0530

    Changed the Color of Login Button to match it with the theme of the website

--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -35,7 +35,7 @@ export const Appbar = () => {
           <ModeToggle />
           {!user ? (
             <Button
-              size={"lg"}
+              className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear"
               onClick={async () => {
                 await signIn();
               }}
