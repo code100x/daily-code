00729d780459ac2a53e3dc54f78a617b857b0a0a
ItsFlash10 <satyamsworld@gmail.com>
2024-08-31 01:08:17 +0530

    feat-604: Added input to change cohort in Admin Panel

--- apps/web/app/admin/layout.tsx
+++ apps/web/app/admin/layout.tsx
@@ -9,10 +9,10 @@ export default async function AdminLayout({ children }: { children: ReactNode })
 
   if (!session?.user?.admin) {
     return (
-      <div className="w-full min-h-screen flex items-center justify-center gap-4 p-4">
+      <div className="flex min-h-screen w-full items-center justify-center gap-4 p-4">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="flex flex-col items-center space-y-2">
-            <LockClosedIcon className="w-14 h-14" />
+            <LockClosedIcon className="h-14 w-14" />
             <div className="space-y-2">
               <h1 className="text-3xl font-bold">Access Denied</h1>
               <p className="text-sm text-gray-500 md:text-base dark:text-gray-400">
@@ -22,7 +22,7 @@ export default async function AdminLayout({ children }: { children: ReactNode })
           </div>
           <div className="flex flex-col gap-2 min-[400px]:flex-row">
             <Link
-              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200  bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
               href="/"
             >
               Return to the homepage
--- apps/web/components/utils.tsx
+++ apps/web/components/utils.tsx
@@ -263,6 +263,7 @@ export async function updateTrack(
     selectedCategory?: string[];
     problems?: { problem: Prisma.ProblemCreateManyInput; sortingOrder: number }[];
     hidden: boolean;
+    cohort?: number;
   }
 ) {
   try {
@@ -276,6 +277,7 @@ export async function updateTrack(
         description: data.description,
         image: data.image,
         hidden: data.hidden,
+        cohort: data.cohort,
       },
     });
     await db.trackCategory.deleteMany({
