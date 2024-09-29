d8c7c523d6243bf547e0d7e124b06c9997eeb16e
Abhi-y2003 <226330001@gkv.ac.in>
2024-09-29 17:07:36 +0530

    Improved pagination

--- apps/web/components/Tracks.tsx
+++ apps/web/components/Tracks.tsx
@@ -113,6 +113,8 @@ export const Tracks = ({ tracks, categories }: TracksWithCategoriesProps) => {
 
   const totalPages = Math.ceil(filteredTracks.length / tracksPerPage);
 
+  let lastPage = totalPages;
+
   const trackAnimation = {
     hidden: { opacity: 1 },
     show: {
@@ -229,11 +231,13 @@ export const Tracks = ({ tracks, categories }: TracksWithCategoriesProps) => {
         <Pagination>
           <PaginationContent>
             <PaginationItem className="cursor-pointer">
-              <PaginationPrevious
-                onClick={() => {
-                  setCurrentPage((prev) => Math.max(prev - 1, 1));
-                }}
-              />
+              {
+                currentPage === 1? (<div></div>) : (<PaginationPrevious
+                  onClick={() => {
+                    setCurrentPage((prev) => Math.max(prev - 1, 1));
+                  }}
+                />) 
+              }
             </PaginationItem>
             {Array.from({ length: totalPages }).map((_, index) => (
               <PaginationItem key={index} className="cursor-pointer">
@@ -250,12 +254,14 @@ export const Tracks = ({ tracks, categories }: TracksWithCategoriesProps) => {
 
             <PaginationItem
             className="cursor-pointer"
-            >
-              <PaginationNext
+            >{
+              currentPage === lastPage ? (<div></div>) : (<PaginationNext
                 onClick={() => {
                   setCurrentPage((prev) => Math.min(prev + 1, totalPages));
                 }}
-              />
+              />) 
+            }
+              
             </PaginationItem>
           </PaginationContent>
         </Pagination>
