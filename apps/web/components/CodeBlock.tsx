a1d77b27605a719da1f4be8da7d5552d9a77df78
dhakaneyogesh <148894509+dhakaneyogesh@users.noreply.github.com>
2025-09-26 17:10:41 +0530

    fix: dark mode toggle not working-for-copy

--- apps/web/components/CodeBlock.tsx
+++ apps/web/components/CodeBlock.tsx
@@ -32,7 +32,7 @@ export default function CodeBlock({ block }: { block: any }) {
         <code className="language-javascript">{code}</code>
       </pre>
       <button className="text-gray-500 p-2 absolute top-2 right-2 hover:opacity-80" onClick={handleCopyClick}>
-        <Copy className="size-5 text-primary/50" />
+        <Copy className="size-5 " />
       </button>
     </div>
   );
